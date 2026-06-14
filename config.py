019e9a6375492ece6280690d3da53fd30bb50db4
DiogenesOfSinope <11579309+DiogenesOfSinope@users.noreply.github.com>
2026-06-14 14:29:38 -0400

    Latest.

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 CAN_CHANNEL = "can0"
 HOST_ID = 0xFD # 0xFD is generally the preferred default CAN ID for the host. Lower ID numbers have priority, ensuring that the motors' feedback wins. 0xFD (253 in decimal) is unlikely to clash with commonly used motor CAN IDs.
 JOINT_CONFIG = {
-    'hip':   {'id': 1, 'direction': -1.0, 'pos_limits': (-0.52, 1.57), 'vel_limits':(-20.94, 20.94)}, # pos_limits: radians
-    'thigh': {'id': 2, 'direction': -1.0, 'pos_limits': (-1.83, 0.0), 'vel_limits':(-20.94, 20.94)},  # vel_limits: radians/second
+    'hip':   {'id': 1, 'direction': 1.0, 'pos_limits': (-0.785, 0.785), 'vel_limits':(-20.94, 20.94)}, # pos_limits: radians
+    'thigh': {'id': 2, 'direction': 1.0, 'pos_limits': (-1.57, 0.261), 'vel_limits':(-20.94, 20.94)},  # vel_limits: radians/second
     'knee':  {'id': 3, 'direction': -1.0, 'pos_limits': (-1.57, 0.0), 'vel_limits':(-20.94, 20.94)}
 }
 RS03_LIMITS = {
@@ -14,11 +14,11 @@ RS03_LIMITS = {
     'T_MIN': -60.0,
     'T_MAX': 60.0
 }
-KP_GAIN = 60.0
-KD_GAIN = 4.0
+KP_GAIN = 6.0
+KD_GAIN = 0.4
 
 ### MODEL CONFIG ###
-CYCLE_PERIOD = 1.5
+CYCLE_PERIOD = 2.0
 MODEL_PATH = "policy.onnx"
 NUM_JOINTS = 3
 LOOP_RATE_HZ = 200
@@ -27,5 +27,5 @@ POLICY_UPDATE_INTERVAL = 4
 LPF_ALPHA = 1.0
 ACTION_LPF_ALPHA = 1.0
 HISTORY_LEN = 10
-ACTION_SCALE = 0.25
+ACTION_SCALE = 1.0
 DEFAULT_POS = [0.0, 0.0, 0.0]
